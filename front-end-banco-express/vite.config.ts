4302ec55c410e1ce66449da874e2f345e54bef89
RainBWP <114127654+RainBWP@users.noreply.github.com>
2025-05-07 12:14:11 -0600

    env

--- front-end-banco-express/vite.config.ts
+++ front-end-banco-express/vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost/banconet/api.php', // matches your backend server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // removes '/api' prefix
-      }
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': {
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '') // removes '/api' prefix
+          }
+          }
+        },
+        preview: {
+          allowedHosts: [
+          env.VITE_ALLOWED_HOST // uses only env variable
+      ]
     }
-  },preview: {
-    allowedHosts: [
-      '9e5d-2806-2f0-7001-804b-a173-1cc9-cd00-c750.ngrok-free.app'
-    ]
-  }
-})
+  };
+});
